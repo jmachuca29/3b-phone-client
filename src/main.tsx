a4afe9eca74b45729eaf44211f3a1284398b3570
Paul Vega <paulmax951@gmail.com>
2024-04-15 12:43:55 -0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
