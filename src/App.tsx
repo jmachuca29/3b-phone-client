f597a08d8cdafa4979e9bf11674a602a362c8989
Paul Vega <paulmax951@gmail.com>
2024-04-15 00:01:43 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import Button from '@mui/material/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button variant="contained">Hello world</Button>
     </>
   )
 }
