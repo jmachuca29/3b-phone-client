f23daf5a2847808e8be5dd2e4a387b0bac81608d
Paul Vega <paulmax951@gmail.com>
2024-04-15 23:58:36 -0500

    update

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,21 +1,23 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+import { Visibility } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const AppPage = () => {
+  const navigate = useNavigate();
   const { isPending, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () =>
@@ -30,25 +32,67 @@ const AppPage = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid xs={12}>
-          <Typography variant="h3" gutterBottom>
-            h3. Heading
-          </Typography>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <Typography variant="h3" gutterBottom>
+              h3. Heading
+            </Typography>
+          </Grid>
+          <Grid xs={12}>
+            <TextField
+              fullWidth
+              label="Buscar un modelo..."
+              id="outlined-start-adornment"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      edge="end"
+                    >
+                      <Visibility />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid container xs={12}>
+            {data.map((element: any, index: any) => {
+              return (
+                <Grid xs={4} key={index}>
+                  <Card>
+                    <CardActionArea
+                      onClick={() => {
+                        console.log('redirect')
+                        navigate('/trade-in');
+                      }}
+                    >
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Lizards are a widespread group of squamate reptiles,
+                          with over 6,000 species, ranging across all continents
+                          except Antarctica
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
         </Grid>
-        <Grid xs={12}>
-          <Item>xs=12</Item>
-        </Grid>
-        <Grid container xs={12}>
-          {data.map(() => {
-            return (
-              <Grid xs={4}>
-                <Item>xs=4</Item>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Grid>
+      </Container>
     </Box>
   );
 };
--- src/routes/sections.tsx
+++ src/routes/sections.tsx
@@ -1,9 +1,10 @@
 import { lazy, Suspense } from 'react';
-import { Outlet, useRoutes } from 'react-router-dom';
+import { Navigate, Outlet, useRoutes } from 'react-router-dom';
 import { DashboardLayout } from 'src/layouts/DashboardLayout';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
-
+export const TradeInPage = lazy(() => import('src/pages/trade-in'));
+export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
 
@@ -18,17 +19,18 @@ export default function Router() {
         </DashboardLayout>
       ),
       children: [
-        { element: <IndexPage />, index: true }
+        { element: <IndexPage />, index: true },
+        { path: 'trade-in', element: <TradeInPage /> },
       ],
     },
-    // {
-    //   path: '404',
-    //   element: <Page404 />,
-    // },
-    // {
-    //   path: '*',
-    //   element: <Navigate to="/404" replace />,
-    // },
+    {
+      path: '404',
+      element: <Page404 />,
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
   ]);
 
   return routes;
