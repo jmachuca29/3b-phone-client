fcd1d8f7472a47d76e8f180582bc88f91fa9f59e
Paul Vega <paulmax951@gmail.com>
2024-09-27 18:32:55 -0500

    update

--- src/components/Accesories/Accesories.tsx
+++ src/components/Accesories/Accesories.tsx
@@ -1,60 +1,77 @@
 import {
-  Box,
-  Button,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-  StepContent,
-  StepLabel,
+    Box,
+    Button,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+    StepContent,
+    StepLabel,
 } from "@mui/material";
 import React from "react";
+import { useForm } from "react-hook-form";
 import useAppStore from "src/store/store";
 
 export const Accesories = ({ handleNext, handleBack }: any) => {
-  const [setFn] = useAppStore((state) => [state.setFn, state.survey]);
-  const [value, setValue] = React.useState<any>('');
+    const [setFn] = useAppStore((state) => [state.setFn, state.survey]);
+    const { handleSubmit, setValue, formState: { errors }, register, trigger, watch } = useForm({
+        defaultValues: {
+            boxIncluded: ''
+        }
+    });
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue((event.target as HTMLInputElement).value)
-  };
+    const boxIncluded = watch("boxIncluded");
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = (event.target as HTMLInputElement).value
+        setValue("boxIncluded", value);
+        trigger("boxIncluded");
+    };
 
-  const handleSubmit = () => {
-    setFn.setOriginalBox(value === "true");
-    handleNext();
-  };
+    const onSubmit = (data: any) => {
+        const value = data.boxIncluded
+        setFn.setOriginalBox(value === "true");
+        handleNext();
+    };
 
-  return (
-    <>
-      <StepLabel>¿Incluyes caja original?</StepLabel>
-      <StepContent>
-        <Box sx={{ mb: 2 }}>
-          <RadioGroup
-            row
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
-            value={value}
-            onChange={handleChange}
-          >
-            <FormControlLabel value="true" control={<Radio />} label="Si" />
-            <FormControlLabel value="false" control={<Radio />} label="No" />
-          </RadioGroup>
-        </Box>
-        <Box sx={{ mb: 2 }}>
-          <div>
-            <Button
-              variant="contained"
-              onClick={handleSubmit}
-              sx={{ mt: 1, mr: 1 }}
-            >
-              Continuar
-            </Button>
-            <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
-              Atras
-            </Button>
-          </div>
-        </Box>
-      </StepContent>
-    </>
-  );
+    return (
+        <>
+            <StepLabel>¿Incluyes caja original?</StepLabel>
+            <StepContent>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Box sx={{ mb: 2 }}>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                            value={boxIncluded}
+                            onChange={handleChange}
+                        >
+                            <FormControlLabel value="true" control={<Radio />} label="Si" />
+                            <FormControlLabel value="false" control={<Radio />} label="No" />
+                        </RadioGroup>
+                    </Box>
+                    <input
+                        type="hidden"
+                        {...register("boxIncluded", { required: "Debes seleccionar al menos una opcion." })}
+                    />
+                    {errors.boxIncluded && <span style={{ color: 'red' }}>{errors.boxIncluded.message}</span>}
+                    <Box sx={{ mb: 2 }}>
+                        <div>
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                sx={{ mt: 1, mr: 1 }}
+                            >
+                                Continuar
+                            </Button>
+                            <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                                Atras
+                            </Button>
+                        </div>
+                    </Box>
+                </form>
+
+            </StepContent>
+        </>
+    );
 };
